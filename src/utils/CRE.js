7a95811681284970de2160afc1631199b47da92c
Obryvalin <67143501+Obryvalin@users.noreply.github.com>
2020-08-17 14:34:14 +0300

    sw0650 changes

--- src/utils/CRE.js
+++ src/utils/CRE.js
@@ -1,4 +1,6 @@
 const request = require("easy-soap-request");
+const validate = require("./validate.js");
+const log = require("./log");
 const fs = require("fs");
 const xml = require("./xml");
 
@@ -10,25 +12,30 @@ const fillTemplate = (propObj, template) => {
   for (var prop in propObj) {
     template = template.replace("@" + prop + "@", propObj[prop]);
   }
-
   return template;
 };
 
-const  makeRequest = async (url,headers,body,timeout)=>{
-  result = await request({ url, headers, xml:body, timeout })
-  
-  return result.response
-}
+const makeRequest = async (url, headers, body, timeout) => {
+  result = await request({ url, headers, xml: body, timeout });
+  return result.response;
+};
 
 const requestSPEXT = (reqdata, callback) => {
+  if (!validate.INN(reqdata.inn)) {
+    if (callback) {
+      callback("Bad INN", undefined);
+    }
+  }
+
   const body = fillTemplate(reqdata, fillTemplate(CREoptions, templateXML));
   // console.log(body);
   const headers = {
     "Content-Type": "text/xml;charset=UTF-8",
-    soapAction: "urn:getBusiness",
+    soapAction: "urn:getBusiness"
   };
+
   resp = makeRequest(url, headers, body, timeout)
-    .then((soapedRes) => {
+    .then(soapedRes => {
       if (soapedRes.statusCode != 200) {
         callback("Error statusCode " + soapedRes.statusCode, undefined);
       }
@@ -52,15 +59,13 @@ const requestSPEXT = (reqdata, callback) => {
         });
       }
     })
-    .catch((error) => {
+    .catch(error => {
       console.log("err\n" + error);
       callback(error, undefined);
     });
 };
 
-
 module.exports = {
   fillTemplate: fillTemplate,
-
-  requestSPEXT: requestSPEXT,
+  requestSPEXT: requestSPEXT
 };
